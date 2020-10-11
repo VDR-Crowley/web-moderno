4dfdea53e955512d85e2d04b6788f9cc1b455ca1
Vando dos reis <vandodosreis2001@gmail.com>
2020-10-11 04:21:34 -0300

    TemplateString

---
+++ exercicios-Web/fundamentos/templateStrings.js
@@ -0,0 +1,7 @@
+const name = 'Vando'
+
+console.log(`My name ${name}`) // interpolação/interpretação
+
+const frase = texto => texto.toUpperCase() 
+
+console.log(`Apenas um ${frase('nada de mais')}`)
