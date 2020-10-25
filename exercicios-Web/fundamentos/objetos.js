dc640a339c8c3b7cd6683853c557a23c3e30a1fe
Vando dos reis <vandodosreis2001@gmail.com>
2020-10-25 14:09:19 -0300

    Entendendo os conceitos de objetos

---
+++ exercicios-Web/fundamentos/objetos.js
@@ -0,0 +1,21 @@
+const games = {}
+
+games.name = 'Halo'
+games.genero = 'Ficcao Cientifica' 
+
+games['codigo'] = 4523615463  // games['codigo de barra'] Evitar atributos com espaço
+
+console.log(games)
+
+// {} - é uma forma literal de criar um objeto
+// Objeto é uma coleção de pares de chave e valor, os nomes dos objetos devem ser unicos
+// no javascript vc pode ir definindo como o objeto vai se comportar dinamicamente 
+
+// Anotação literal
+
+const filmes = {
+  name: 'Homem de ferro',
+  desc: 'bla bla...'
+}
+
+console.log(filmes)
