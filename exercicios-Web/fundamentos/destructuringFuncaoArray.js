303fbc51ea91626254c118edea1f31a99ac2f233
Vando dos reis <vandodosreis2001@gmail.com>
2020-11-08 17:35:49 -0300

    Praticando destructuring com funçao e array e objetos

---
+++ exercicios-Web/fundamentos/destructuringFuncaoArray.js
@@ -0,0 +1,7 @@
+function Rand( [min = 0, max = 100]) {
+  if(min > max) [min, max] = [max, min]
+  const valor = Math.random() * (max - min) + min
+  return Math.floor(valor)
+}
+
+console.log( Rand([45, 50]) )
