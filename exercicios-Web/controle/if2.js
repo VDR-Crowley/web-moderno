e5a149faf20c9466c219f81086ee0fff665eef3e
Vando dos reis <vandodosreis2001@gmail.com>
2020-11-28 15:49:14 -0300

    IF2

---
+++ exercicios-Web/controle/if2.js
@@ -0,0 +1,15 @@
+function teste1(num) {
+  if(num > 7) 
+  console.log(num);
+  console.log('Cheguei ate aqui');
+}
+
+teste1(4)
+
+function teste2(num) {
+  if(num < 5);{
+    console.log(num)
+  }
+}
+
+teste2(8)
