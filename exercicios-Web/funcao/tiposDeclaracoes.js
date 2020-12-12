81bb35cf24d6f0427f9e3033a5b9c81beaedae53
Vando dos reis <vandodosreis2001@gmail.com>
2020-12-12 18:16:46 -0300

    Tipos de declaraçao de funçoes

---
+++ exercicios-Web/funcao/tiposDeclaracoes.js
@@ -0,0 +1,17 @@
+console.log(soma(5, 5));
+// function declaration
+function soma(x, y) {
+  return x + y;
+}
+
+// function expression 
+const sub = function(x, y) {
+  return x - y;
+}
+console.log(sub(2, 5));
+
+// named function expression
+const multy = function multy(x, y) {
+  return x * y;
+}
+console.log(multy(5, 5));
