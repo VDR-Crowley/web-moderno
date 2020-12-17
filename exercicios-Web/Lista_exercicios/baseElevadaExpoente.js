638415dcbe1382bb98cc54c326753e70000f6380
Vando dos reis <vandodosreis2001@gmail.com>
2020-12-17 19:58:40 -0300

    exercicio base elevada ao expoente

---
+++ exercicios-Web/Lista_exercicios/baseElevadaExpoente.js
@@ -0,0 +1,10 @@
+/*
+  03)​ Crie uma função que recebe dois parâmetros, 
+  base e expoente, e retorne a base elevada ao expoente.
+*/
+
+const calculo = (base, expoente) => {
+  return base ** expoente;
+}
+
+console.log(calculo(2, 2));
