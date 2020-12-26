5318f47b32dce535bec41977a07acdf14c267ee7
Vando dos reis <vandodosreis2001@gmail.com>
2020-12-26 20:42:27 -0300

    OO

---
+++ exercicios-Web/objeto/oo.js
@@ -0,0 +1,25 @@
+// CÓDIGO NÃO EXECUTAVEL
+
+// paradigma procedural
+function processos(valor1, valor2, valor3) {
+  return valor1 + valor2 + valor3;
+}
+processos(1, 2, 3);
+
+
+// paradigma OO
+objeto = {
+  valor1,
+  valor2,
+  valor3,
+  processos(){
+    // ...
+  }
+};
+objeto.processos() // o foco passou a ser o objeto
+
+// principios importantes:
+// 1. abstração
+// 2. encapsulamento
+// 3. herança (prototype)
+// 4. polimorfismo
